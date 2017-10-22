ea871dae7accb8535c2bdce8eefd63aac950ce30
weltonvaz <weltonvaz@gmail.com>
2017-10-22 17:49:30 -0200

    reinitialized files

---
+++ perfis/urls.py
@@ -0,0 +1,8 @@
+# connectedin/perfis/urls.py
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'perfis.views.index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir')
+)
---
+++ perfis/views.py
@@ -0,0 +1,16 @@
+# connectedin/perfis/views.py
+
+from django.shortcuts import render
+from perfis.models import Perfil
+
+def index(request):
+    return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+
+    if perfil_id == '1':
+        perfil = Perfil('Flavio Almeida', 'flavio@flavio.com.br', '777777', 'Caelum')
+
+    return render(request, 'perfil.html')
